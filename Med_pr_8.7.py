9bcf40cc722693b9169b65ab2947bae32db8e478
viktortatarnikov <vtok241098@mail.ru>
2023-05-30 11:48:44 +0300

    Task number 8.7.4

--- Med_pr_8.7.py
+++ Med_pr_8.7.py
@@ -1,2 +1,19 @@
-#Task number three
-print(*sorted((set(int(ind) for ind in input().split())  & set(int(ind) for ind in input().split()) ) - set(int(ind) for ind in input().split()) , reverse=True))
+#Task number four
+set1 = set(int(ind) for ind in input().split())
+set2 = set(int(ind) for ind in input().split())
+set3 = set(int(ind) for ind in input().split())
+set4 = []
+set5 = set1 | set2 | set3
+
+for i in set5:
+    count = 0
+    if i in set1:
+        count+=1
+    if i in set2:
+        count+=1
+    if i in set3:
+        count+=1
+    if count < 3:
+        set4.append(i)
+
+print(*sorted(set4))
