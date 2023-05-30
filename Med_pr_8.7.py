0da0404aade38106906ed1c86db11354537bc10f
viktortatarnikov <vtok241098@mail.ru>
2023-05-30 12:11:42 +0300

    Task number 8.7.6

--- Med_pr_8.7.py
+++ Med_pr_8.7.py
@@ -1,6 +1,7 @@
-#Task number five
+#Task number six
 set1 = set(int(ind) for ind in input().split())
 set2 = set(int(ind) for ind in input().split())
 set3 = set(int(ind) for ind in input().split())
+set4 = set(range(11))
 
-print(*sorted(set3 - (set3&set2 | set3&set1), reverse=True))
+print(*sorted(set4 - set3 -set2 - set1))
