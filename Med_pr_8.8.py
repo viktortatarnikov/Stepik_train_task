7fb5c2d0ec082a3a1ecfabe3c31b6ea9799a34f0
viktortatarnikov <vtok241098@mail.ru>
2023-05-31 14:52:36 +0300

    Task number 9.10???

--- Med_pr_8.8.py
+++ Med_pr_8.8.py
@@ -1,7 +1,2 @@
-m, n = int(input()), int(input())
-m_set = {input() for _ in range(m)}
-n_set = {input() for _ in range(n)}
-if len(m_set - n_set) == len(n_set - m_set) == 0:
-    print('NO')
-else:
-    print(len(m_set - n_set) + len(n_set - m_set))
+m, n = set(input().split()), set(input().split())
+print(*sorted(m|n))
