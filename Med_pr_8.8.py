e9d6af411108092262e7f083613bd8eade3f47a5
viktortatarnikov <vtok241098@mail.ru>
2023-05-31 14:37:37 +0300

    Task number 9.5

--- Med_pr_8.8.py
+++ Med_pr_8.8.py
@@ -1,6 +1,9 @@
-n_list = [input() for i in range(int(input()))]
-
-if input() in n_list:
-    print('REPEAT')
-else:
-    print('OK')
+m, n = int(input()), int(input())
+m_list = [input() for _ in range(m)]
+n_list = [input() for _ in range(n)]
+n_set = set(n_list)
+for i in n_list:
+    if i in m_list:
+        print('YES')
+    else:
+        print('NO')
