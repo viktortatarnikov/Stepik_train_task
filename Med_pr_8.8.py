1165a5d1bc0f8d94947f71c42986d5b45ce2c908
viktortatarnikov <vtok241098@mail.ru>
2023-05-31 15:01:41 +0300

    Task number 9.11???

--- Med_pr_8.8.py
+++ Med_pr_8.8.py
@@ -1,2 +1,7 @@
-m, n = set(input().split()), set(input().split())
-print(*sorted(m|n))
+m, n = int(input()), int(input())
+list1 = [input() for _ in range(m + n)]
+set1 = set(list1)
+if len(set1) == len(list1):
+    print('NO')
+else:
+    print(len(list1) - 2*(len(list1) - len(set1)))
