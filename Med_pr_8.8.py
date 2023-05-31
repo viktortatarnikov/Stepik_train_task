79d6e775cd5d8eb75531e00fb0a2bfa7fa8ad4fb
viktortatarnikov <vtok241098@mail.ru>
2023-05-31 14:50:27 +0300

    Task number 9.9???

--- Med_pr_8.8.py
+++ Med_pr_8.8.py
@@ -1,6 +1,7 @@
-m, n = {int(i) for i in input().split()}, {int(i) for i in input().split()}
-s = m & n
-if len(s) == 0:
-    print('BAD DAY')
+m, n = int(input()), int(input())
+m_set = {input() for _ in range(m)}
+n_set = {input() for _ in range(n)}
+if len(m_set - n_set) == len(n_set - m_set) == 0:
+    print('NO')
 else:
-    print(*sorted(s, reverse=True))
+    print(len(m_set - n_set) + len(n_set - m_set))
