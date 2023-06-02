6866cd4a4aaa21a43b5b154d637d828e8a6decac
viktortatarnikov <vtok241098@mail.ru>
2023-06-02 15:48:31 +0300

    Med pr 10.3.1

---
+++ Med_pr_10.3.py
@@ -0,0 +1,10 @@
+s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
+result = {}
+for i in s.split():
+    result[i] = result.get(i, 0) + 1
+a = []
+for i in result:
+    if result[i] == max(result.values()):
+        a.append(i)
+a.sort()
+print(a[0])
