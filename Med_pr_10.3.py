88f809bcdbcac7e38ae19f3e1a4c808d5159c4b8
viktortatarnikov <vtok241098@mail.ru>
2023-06-08 17:04:49 +0300

    Med_pr_10.3.4

--- Med_pr_10.3.py
+++ Med_pr_10.3.py
@@ -1,18 +1,12 @@
-# Полу-рабочий код
-text = [word.strip('!&,.:;-') for word in input().lower().split()]
-words ={}
-for w in text:
-    words[w] = words.get(w, 0) + 1
-for ind in words.items():
-    if ind[1] == min(words.values()):
-        print(ind[0])
-        break
-
-# Рабочий код (проходит все тесты)
-# dct = {}
-# lst = [word.strip('.,!?:;-') for word in input().lower().split()]
-# for word in lst:
-#     dct[word] = dct.get(word, 0) + 1
-# lst = [(value, key) for key, value in dct.items()]
-# lst.sort()
-# print(lst[0][1])
+# Не решил
+lst = input().split()
+res = []
+cnt = {}
+for c in lst:
+    if c in cnt:
+        res.append(c + '_' + str(cnt[c]))
+        cnt[c] += 1
+    else:
+        res.append(c)
+        cnt[c] = 1
+print(*res, sep=' ')
