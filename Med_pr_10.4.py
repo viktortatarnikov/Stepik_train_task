4dc72dee561dc8bdd6362a12392109c008bca00c
viktortatarnikov <vtok241098@mail.ru>
2023-06-19 11:43:26 +0300

    Med_pr_10.4.5.py

--- Med_pr_10.4.py
+++ Med_pr_10.4.py
@@ -1,10 +1,9 @@
-# Варинат 1
-dic = {}
-
+dic = {i[0]: i[1:] for i in [input().split() for _ in range(int(input()))]}
+req = []
 for _ in range(int(input())):
-    key, veriable = input().split()
-    dic[key] = veriable
-    dic[veriable] = key
-print(dic[input()])
-
+    req.append(input())
+for city in req:
+     for key, veriables in dic.items():
+         if city in veriables:
+             print(key)
 
