6282256d0204c1357b9ba1021696f887983e5ba2
viktortatarnikov <vtok241098@mail.ru>
2023-06-19 11:24:23 +0300

    Med_pr_10.4.4.py

--- Med_pr_10.4.py
+++ Med_pr_10.4.py
@@ -1,30 +1,10 @@
-# Моё решение 
-d = {}
+# Варинат 1
+dic = {}
 
-lst_word1 = [a.lower().strip('.,!?:;-') for a in input().split()]
-word1 = ''
-for a in lst_word1:
-    word1 += a
+for _ in range(int(input())):
+    key, veriable = input().split()
+    dic[key] = veriable
+    dic[veriable] = key
+print(dic[input()])
 
-lst_word2 = [a.lower().strip('.,!?:;-') for a in input().split()]
-word2 = ''
-for a in lst_word2:
-    word2 += a
 
-for c in word1:
-    d[c] = d.get(c, 0) + 1
-for c in word2:
-    d[c] = d.get(c, 0) - 1    
-
-print(('NO', 'YES')[set(d.values()) == {0}])
-
-# Умное решение
-# def s(word):
-#     res = {}
-#     for i in word.lower():
-#         if i.isalpha():
-#             res[i] = res.get(i, 0) + 1
-#     return res
-#
-#
-# print(("NO", "YES")[s(input()) == s(input())])
