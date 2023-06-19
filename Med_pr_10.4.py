9e3734bc1fe40e0041dd2f6f8c1c63a994acf6fe
viktortatarnikov <vtok241098@mail.ru>
2023-06-19 12:48:01 +0300

    Med_pr_10.4.7.py

--- Med_pr_10.4.py
+++ Med_pr_10.4.py
@@ -1,21 +1,9 @@
 # Моё решение
-dic = {}
-for _ in range(int(input())):
-    value, key = input().lower().split()
-    dic[key] = dic.get(key, "") + (value + ' ')
+s_m = input()
+dic = {int(i[1]): i[0] for i in [input().split(': ') for _ in range(int(input()))]}
+s_m_dic = {}
+for litter in s_m:
+    s_m_dic[litter] = s_m_dic.get(litter, 0) + 1
+for simb in s_m:
 
-
-req = [input().lower() for _ in range(int(input()))]
-
-for name in req:
-    if name in dic.keys():
-         print(dic[name])
-    else:
-         print('абонент не найден')
-# Красивое решение
-# dct = {}
-# for _ in range(int(input())):
-#     phone, name = input().lower().split()
-#     dct.setdefault(name, []).append(phone)
-# for _ in range(int(input())):
-#     print(*dct.get(input().lower(), ['абонент не найден']))
+    print(dic[s_m_dic[simb]], end='')
