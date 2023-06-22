c35111d53b834af46b27460d987d1e6a3840ec14
viktortatarnikov <vtok241098@mail.ru>
2023-06-22 17:18:17 +0300

    Med_pr_12.2.7

--- Med_pr_12.2.py
+++ Med_pr_12.2.py
@@ -1,12 +1,9 @@
 import random
-bk = random.sample(range(1, 76), 25)
-bk[12] = 0
-for i in (0, 5, 10, 15, 20):
-    print(*bk[i: i + 5], end='')
-    print()
-
-# Чуть более красивое решение
-# from random import sample
-#
-# nums = iter(sample(list(range(1, 75)), 24))
-# [print(*('0  ' if i == j == 2 else str(next(nums)).ljust(3) for j in range(5))) for i in range(5)]
+n = int(input())
+students = []
+for _ in range(n):
+    students.append(input())
+random.shuffle(students)
+for ind in range(len(students) - 1):
+    print(students[ind], '-', students[ind + 1])
+print(students[-1], '-', students[0])
