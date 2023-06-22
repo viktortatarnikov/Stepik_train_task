71e93b57584cf6b4d12adf8401a94e6df59dda45
viktortatarnikov <vtok241098@mail.ru>
2023-06-22 17:46:02 +0300

    Med_pr_12.2.8

--- Med_pr_12.2.py
+++ Med_pr_12.2.py
@@ -1,9 +1,29 @@
 import random
-n = int(input())
-students = []
-for _ in range(n):
-    students.append(input())
-random.shuffle(students)
-for ind in range(len(students) - 1):
-    print(students[ind], '-', students[ind + 1])
-print(students[-1], '-', students[0])
+import string
+a = string.ascii_uppercase.replace('I', '').replace("O", '').replace('L', '')
+
+b = string.ascii_lowercase.replace('i', '')
+b = b.replace('o', '')
+b = b.replace('l', '')
+c = a + b
+d = string.digits.strip('10')
+def generate_password(m):
+    a = string.ascii_uppercase.replace('I', '').replace("O", '').replace('L', '')
+    b = string.ascii_lowercase.replace('i', '')
+    b = b.replace('o', '')
+    b = b.replace('l', '')
+    d = string.digits.strip('10')
+    c = a + b + d
+    ps = [random.choice(a), random.choice(b), random.choice(d)]
+    for ind in range(m - 3):
+        ps.append(random.choice(c))
+    random.shuffle(ps)
+    return ''.join(ps)
+
+
+def generate_passwords(n, m):
+    for _ in range(n):
+        print(generate_password(m))
+
+n, m = int(input()), int(input())
+generate_passwords(n, m)
