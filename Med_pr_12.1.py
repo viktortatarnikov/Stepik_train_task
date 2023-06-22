50ef36ddb5bfefd5c9f92f0e3b12ae3e043af45b
viktortatarnikov <vtok241098@mail.ru>
2023-06-22 12:29:33 +0300

    Med_pr_12.1.2

--- Med_pr_12.1.py
+++ Med_pr_12.1.py
@@ -2,16 +2,4 @@ import random
 
 n = int(input())    # количество попыток
 for _ in range(n):
-    a = random.randint(1, 2)
-    if a == 1:
-        print('Орел')
-    else:
-        print('Решка')
-
-# Красивое решение
-# from random import randint
-#
-# COIN = ['Орел', 'Решка']
-#
-# for _ in range(int(input())):
-#     print(COIN[randint(0, 1)])
+    print(random.randint(1, 6))
