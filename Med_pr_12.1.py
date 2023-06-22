68f86890d9cf5c8441ceaa0d1ec94fcbb2a9af75
viktortatarnikov <vtok241098@mail.ru>
2023-06-22 12:36:11 +0300

    Med_pr_12.1.3

--- Med_pr_12.1.py
+++ Med_pr_12.1.py
@@ -1,5 +1,10 @@
 import random
 
-n = int(input())    # количество попыток
-for _ in range(n):
-    print(random.randint(1, 6))
+length = int(input())    # длина пароля
+for _ in range(length):
+    flag = random.randint(1, 2)
+    if flag == 1:
+        print(chr(random.randint(65, 90)), end='')
+    else:
+        print(chr(random.randint(97, 122)), end='')
+
