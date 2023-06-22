403ccdd9ad085ad3d0c4a245db9d1565c9c8a14e
viktortatarnikov <vtok241098@mail.ru>
2023-06-22 17:02:49 +0300

    Med_pr_12.2.6

--- Med_pr_12.2.py
+++ Med_pr_12.2.py
@@ -1,7 +1,12 @@
 import random
-word = [ind for ind in input()]
-random.shuffle(word)
-print(*word, sep='')
+bk = random.sample(range(1, 76), 25)
+bk[12] = 0
+for i in (0, 5, 10, 15, 20):
+    print(*bk[i: i + 5], end='')
+    print()
 
-
-
+# Чуть более красивое решение
+# from random import sample
+#
+# nums = iter(sample(list(range(1, 75)), 24))
+# [print(*('0  ' if i == j == 2 else str(next(nums)).ljust(3) for j in range(5))) for i in range(5)]
