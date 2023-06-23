e5eacc4e3634d5603b4f241f1e7148c30db514ac
viktortatarnikov <vtok241098@mail.ru>
2023-06-23 17:03:07 +0300

    Med_pr_13.2.7

--- Med_pr_13.2.py
+++ Med_pr_13.2.py
@@ -1,7 +1,10 @@
 from fractions import *
 from  math import*
-n = int(input())
-
-lst = [Fraction(i, n-i) for i in range(n) if Fraction(i, n-i) < 1 and Fraction(i, n-i).numerator + Fraction(i, n-i).denominator == n]
-
-print(max(lst))
+n = set()
+num = int(input())
+for i in range(1, num+1):
+    for j in range(1, num+1):
+        if Fraction(i, j) < 1:
+            n.add(Fraction(i, j))
+for ind in sorted(n):
+    print(ind)
