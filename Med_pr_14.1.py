eb7a7e43bec41ad4ec705d6e6ccee9e1b4d4d4fd
viktortatarnikov <vtok241098@mail.ru>
2023-06-26 12:41:52 +0300

    Med_pr_14.1.1

---
+++ Med_pr_14.1.py
@@ -0,0 +1,18 @@
+import turtle
+
+def squer(side):
+    turtle.left(22)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+    turtle.forward(side)
+    turtle.left(90)
+
+
+
+squer(100)
+squer(100)
+squer(100)
