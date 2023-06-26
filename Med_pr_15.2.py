71e8ae1ea60f50f2aa858433a89198a594d46a76
viktortatarnikov <vtok241098@mail.ru>
2023-06-26 20:02:34 +0300

    Med_pr_15.2.6.py

--- Med_pr_15.2.py
+++ Med_pr_15.2.py
@@ -1,12 +1,5 @@
-def print_products(*ar):
-    res = [ind for ind in ar if type(ind) == str and ind != '']
-    if len(res) != 0:
-        for ind in range(len(res)):
-            print(str(ind + 1) + ')', res[ind])
-    else:
-        print('Нет продуктов')
+def info_kwargs(**kwargs):
+    for key, value in sorted(kwargs.items()):
+        print(key, value, sep=': ')
 
-print_products()
-print_products('Тимур', 'Dfkthf', 'Саша')
-print_products([4], {}, 1, 2, {'Beegeek'}, '')
-print_products('Бананы', [1, 2], ('Stepik',), 'Яблоки', '', 'Макароны', 5, True)
+info_kwargs(first_name='Timur', last_name='Guev', age=28, job='teacher')
