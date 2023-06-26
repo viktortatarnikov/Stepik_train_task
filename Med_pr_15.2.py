4b0edfd058aeacdd5bad5b6dad5011552e41b624
viktortatarnikov <vtok241098@mail.ru>
2023-06-26 19:54:35 +0300

    Med_pr_15.2.5.py

--- Med_pr_15.2.py
+++ Med_pr_15.2.py
@@ -1,10 +1,12 @@
-def greet(name, *ar):
-    res = [ind for ind in ar]
-    words = 'Hello, ' + name
+def print_products(*ar):
+    res = [ind for ind in ar if type(ind) == str and ind != '']
     if len(res) != 0:
-        words += ' and ' + ' and '.join(res)
-        return words + '!'
+        for ind in range(len(res)):
+            print(str(ind + 1) + ')', res[ind])
     else:
-        return words + '!'
-print(greet('Тимур'))
-print(greet('Тимур', 'Dfkthf', 'Саша'))
+        print('Нет продуктов')
+
+print_products()
+print_products('Тимур', 'Dfkthf', 'Саша')
+print_products([4], {}, 1, 2, {'Beegeek'}, '')
+print_products('Бананы', [1, 2], ('Stepik',), 'Яблоки', '', 'Макароны', 5, True)
