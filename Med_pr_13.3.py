eac34fef106d89f6b7656ccb1d332bdf24860861
viktortatarnikov <vtok241098@mail.ru>
2023-06-26 11:39:48 +0300

    Med_pr_13.3.1

--- Med_pr_13.3.py
+++ Med_pr_13.3.py
@@ -1,5 +1,7 @@
-a = complex(input())
-b = complex(input())
-print(a, '+', b, '=', a + b)
-print(a, '-', b, '=', a - b)
-print(a, '*', b, '=', a * b)
+numbers = [3 + 4j, 3 + 1j, -7 + 3j, 4 + 8j, -8 + 10j, -3 + 2j, 3 - 2j, -9 + 9j, -1 - 1j, -1 - 10j, -20 + 15j, -21 + 1j, 1j, -3 + 8j, 4 - 6j, 8 + 2j, 2 + 3j]
+numbers2 = [abs(i) for i in numbers]
+print(numbers[numbers2.index(max(numbers2))], max(numbers2), sep='\n')
+# Красивое решение
+
+# print(max(numbers, key=abs))
+# print(abs(max(numbers, key=abs)))
