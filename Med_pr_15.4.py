7ebeff734b402e09f49cecfda08c23d55ffac02e
viktortatarnikov <vtok241098@mail.ru>
2023-06-27 15:03:44 +0300

    Med_pr_15.4.5.py

--- Med_pr_15.4.py
+++ Med_pr_15.4.py
@@ -1,18 +1,21 @@
-athletes = [('Дима', 10, 130, 35), ('Тимур', 11, 135, 39), ('Руслан', 9, 140, 33), ('Рустам', 10, 128, 30), ('Амир', 16, 170, 70), ('Рома', 16, 188, 100), ('Матвей', 17, 168, 68), ('Петя', 15, 190, 90)]
+import math
 
-def f1(pos):
-    return pos[0]
+def f1(num):
+    return num**2
 
-def f2(pos):
-    return pos[1]
+def f2(num):
+    return num**3
 
-def f3(pos):
-    return pos[2]
+def f3(num):
+    return num**0.5
 
-def f4(pos):
-    return pos[3]
-funcs = [f1, f2, f3, f4]
+def f4(num):
+    return abs(num)
 
-for ind in sorted(athletes, key=funcs[int(input()) - 1]):
-    print(*ind)
+def f5(num):
+    return math.sin(num)
+commands = {'квадрат': f1, 'куб': f2, 'корень': f3, 'модуль': f4, 'синус': f5}
+num = int(input())
+com = commands[input()]
+print(com(num))
 
