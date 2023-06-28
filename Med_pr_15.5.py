87c5e42fa08740f9fe51dd17096acfb8ef76c59e
viktortatarnikov <vtok241098@mail.ru>
2023-06-28 15:35:06 +0300

    Med_pr_15.5.1.py

---
+++ Med_pr_15.5.py
@@ -0,0 +1,15 @@
+from decimal import *
+
+def map(function, items):
+    result = []
+    for item in items:
+        result.append(function(item))
+    return result
+
+def okrug(num):
+    return Decimal(str(num)).quantize(Decimal('1.00'))
+
+
+numbers = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.12013, 23.22222, 90.09873, 45.45, 314.1528, 2.71828, 1.41546]
+
+print(*map(okrug, numbers), sep='\n')
