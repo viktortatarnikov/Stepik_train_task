a576a0ec18b9ec1aa119f9c6e4e7e70b23645469
viktortatarnikov <vtok241098@mail.ru>
2023-06-30 13:10:20 +0300

    Med_pr_15.8.5.py

--- Med_pr_15.8.py
+++ Med_pr_15.8.py
@@ -1,15 +1,2 @@
-is_num = lambda x: True if set(x) <= set('-.1234567890') and x.count('.') <= 1 <= 1 and '-' not in x[1:]\
-    else False
-print(is_num('10.34ab'))
-print(is_num('10.45'))
-print(is_num('-18'))
-print(is_num('-34.67'))
-print(is_num('987'))
-print(is_num('abcd'))
-print(is_num('123.122.12'))
-print(is_num('-123.122'))
-print(is_num('--13.2'))
-
-# Красивое решение
-# is_num = lambda x: x.lstrip("-").replace(".", "", 1).isdecimal()
-
+words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able', 'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+print(*sorted(filter(lambda x: len(x) == 6 , words)))
