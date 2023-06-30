33a5f662c7f9a061aa50e6ec5ec1069d208f64c9
viktortatarnikov <vtok241098@mail.ru>
2023-06-30 12:28:19 +0300

    Med_pr_15.8.3.py

--- Med_pr_15.8.py
+++ Med_pr_15.8.py
@@ -1,10 +1,13 @@
-func = lambda x: True if x[0].lower() == x[-1].lower() == 'a' else False
-print(func('abcd'))
-print(func('bcda'))
-print(func('abcda'))
-print(func('Abcd'))
-print(func('bcdA'))
-print(func('abcdA'))
+is_non_negative_num = lambda x: True if x.replace('.', '', 1).isdigit() and float(x) >= 0 else False
+print(is_non_negative_num('10.34ab'))
+print(is_non_negative_num('10.45'))
+print(is_non_negative_num('-18'))
+print(is_non_negative_num('-34.67'))
+print(is_non_negative_num('987'))
+print(is_non_negative_num('abcd'))
+print(is_non_negative_num('123.122.12'))
+print(is_non_negative_num('123.122'))
 
 # Красивое решение
-# func=lambda x: x[0] in 'aA' and x[-1] in 'aA'
+# is_non_negative_num = lambda s: s.count('.') <= 1 and set(s) <= set('.1234567890')
+
