091dcb0cc25f45b42b54f20e59fad1e9b7c229f9
viktortatarnikov <vtok241098@mail.ru>
2023-06-30 15:53:38 +0300

    Med_pr_15.8.8.py

--- Med_pr_15.8.py
+++ Med_pr_15.8.py
@@ -1,3 +1,2 @@
-data = [(19542209, 'New York'), (4887871, 'Alabama'), (1420491, 'Hawaii'), (626299, 'Vermont'), (1805832, 'West Virginia'), (39865590, 'California'), (11799448, 'Ohio'), (10711908, 'Georgia'), (10077331, 'Michigan'), (10439388, 'Virginia'), (7705281, 'Washington'), (7151502, 'Arizona'), (7029917, 'Massachusetts'), (6910840, 'Tennessee')]
-for ind in sorted(data, key=lambda point: point[1][-1], reverse=True):
-    print(ind[1], ': ', ind[0], sep='')
+data = ['год', 'человек', 'время', 'дело', 'жизнь', 'день', 'рука', 'раз', 'работа', 'слово', 'место', 'лицо', 'друг', 'глаз', 'вопрос', 'дом', 'сторона', 'страна', 'мир', 'случай', 'голова', 'ребенок', 'сила', 'конец', 'вид', 'система', 'часть', 'город', 'отношение', 'женщина', 'деньги']
+print(*sorted(sorted(data), key=lambda x: len(x)))
