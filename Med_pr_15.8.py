9036afdf4eea2200a3850f71c2e1b2337b58e1fd
viktortatarnikov <vtok241098@mail.ru>
2023-07-03 10:25:20 +0300

    Med_pr_15.8.last.py

--- Med_pr_15.8.py
+++ Med_pr_15.8.py
@@ -1,1 +1,11 @@
-print(*list(map(lambda x: 255 - x, [int(x) for x in input().split()])))
+from functools import reduce
+lst = [int(ind) for ind in input().split()]
+x = int(input())
+
+def evaluate(coefficients, x):
+    index = list(range(len(coefficients)))[::-1]
+
+    return reduce(lambda x, y: x + y, map(lambda a, index: a*(x**index), coefficients, index), 0)
+
+
+print(evaluate(lst, x))
