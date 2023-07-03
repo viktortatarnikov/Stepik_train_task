005662c40d811ad230794d4043f4356da6b88deb
viktortatarnikov <vtok241098@mail.ru>
2023-07-03 19:52:45 +0300

    Med_pr_16.3.3.py

--- Med_pr_16.3.py
+++ Med_pr_16.3.py
@@ -1,8 +1,4 @@
-from functools import reduce
+words = 'the world is mine take a look what you have started'.split()
 
-def product_of_odds(data):   # data - список целых чисел
+print(*map(lambda word: '"' + word + '"', words))
 
-    return reduce(lambda a, b: a*b, filter(lambda x: x % 2 == 1, data), 1)
-
-
-print(product_of_odds([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
