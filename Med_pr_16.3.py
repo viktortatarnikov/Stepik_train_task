1f70f7ac3a41fa6f6851e80718cb372e436a5c3a
viktortatarnikov <vtok241098@mail.ru>
2023-07-03 20:15:22 +0300

    Med_pr_16.3.7.py

--- Med_pr_16.3.py
+++ Med_pr_16.3.py
@@ -1,15 +1,9 @@
-def add3(x):
-    return x + 3
+def arithmetic_operation(operation):
+    dct = {'+': lambda x, y: x+y, '-': lambda x, y: x-y, '*': lambda x, y: x*y, '/': lambda x, y: x/y}
+    return dct[operation]
 
 
-def mul7(x):
-    return x * 7
-
-def compose(f, g):
-    return lambda x: f(g(x))
-
-
-print(compose(mul7, add3)(1))
-print(compose(add3, mul7)(2))
-print(compose(mul7, str)(3))
-print(compose(str, mul7)(5))
+add = arithmetic_operation('+')
+div = arithmetic_operation('/')
+print(add(10, 20))
+print(div(20, 5))
