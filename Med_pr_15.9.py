38a8ddfb8811fc523f053a9ae40a60968de49463
viktortatarnikov <vtok241098@mail.ru>
2023-07-03 12:02:02 +0300

    Med_pr_15.9.6.py

--- Med_pr_15.9.py
+++ Med_pr_15.9.py
@@ -1,8 +1,7 @@
-a, b = int(input()), int(input())
-for ind in range(a, b+1):
-    if all(map(lambda x: ind % int(x) == 0 if x != '0' else False, str(ind))):
-        print(ind, end=' ')
-
-# Можно заменить в условии на полноценную функцию
-# def chek(num):
-#    return all(map(lambda x: ind % int(x) == 0 if x != '0' else False, str(num)))
+def chek(password):
+    return all([any(map(lambda x: x.isdigit(), password)), any(map(lambda x: x.isalpha(), password)), len(password) >= 7,
+                any(map(lambda x: x.isupper(), password)), any(map(lambda x: x.islower(), password))])
+if chek(input()):
+    print('YES')
+else:
+    print('NO')
