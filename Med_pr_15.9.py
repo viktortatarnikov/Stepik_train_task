b5f89b891a2ca697a36b0ca4a4762bcf4d30de49
viktortatarnikov <vtok241098@mail.ru>
2023-07-03 11:50:41 +0300

    Med_pr_15.9.5.py

--- Med_pr_15.9.py
+++ Med_pr_15.9.py
@@ -1,1 +1,8 @@
-print(all(map(lambda x: x.isdigit() and 0 <= int(x) <= 255, input().split('.'))))
+a, b = int(input()), int(input())
+for ind in range(a, b+1):
+    if all(map(lambda x: ind % int(x) == 0 if x != '0' else False, str(ind))):
+        print(ind, end=' ')
+
+# Можно заменить в условии на полноценную функцию
+# def chek(num):
+#    return all(map(lambda x: ind % int(x) == 0 if x != '0' else False, str(num)))
