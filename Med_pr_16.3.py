9d18eed2eb4fae7d618f49deea9545b5728117e3
viktortatarnikov <vtok241098@mail.ru>
2023-07-03 19:54:26 +0300

    Med_pr_16.3.4.py

--- Med_pr_16.3.py
+++ Med_pr_16.3.py
@@ -1,4 +1,3 @@
-words = 'the world is mine take a look what you have started'.split()
-
-print(*map(lambda word: '"' + word + '"', words))
+numbers = [18, 191, 9009, 5665, 78, 77, 45, 23, 19991, 908, 8976, 6565, 5665, 10, 1000, 908, 909, 232, 45654, 786]
+print(*filter(lambda num: str(num) != str(num)[::-1], numbers))
 
