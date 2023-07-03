e5816d458fefce32da59f45181d14b2df42110d1
viktortatarnikov <vtok241098@mail.ru>
2023-07-03 21:05:44 +0300

    Med_pr_16.3.10.py

--- Med_pr_16.3.py
+++ Med_pr_16.3.py
@@ -1,14 +1,12 @@
-words = [input() for word in range(int(input()))]
+words = [input().split('.') for word in range(int(input()))]
 
-def gem(word):
-    total = 0
-    for i in word:
-        total += ord(i.upper()) - ord('A')
-    return total
+# def gem(word):
+#     total = 0
+#     for i in word:
+#         total += ord(i.upper()) - ord('A')
+#     return total
+for ind in range(len(words)):
+    words[ind] = [int(w) for w in words[ind]]
 
-words.sort()
-print(*sorted(words, key=gem), sep='\n')
-
-words = ['basis', 'after', 'chief', 'agenda']
-words.sort()
-print(*sorted(words, key=gem))
+for word in sorted(words):
+    print(*word, sep='.')
