91e52acc1c7dbe87f5b8a4aeae767319bcbc55d2
viktortatarnikov <vtok241098@mail.ru>
2023-07-03 16:49:56 +0300

    Med_pr_16.1.1.py

--- Med_pr_16.1.py
+++ Med_pr_16.1.py
@@ -1,15 +1,16 @@
-def generate_letter(mail, name, date, time, place, teacher='Тимур Гуев', number=17):
+def pretty_print(data, side='-', delimiter='|'):
+    data = [str(ind) for ind in data]
+    res = delimiter
+    for word in data:
+        res += ' ' + word + " " + delimiter
+    side1 = ' ' + ''.join([side for _ in range(len(res) - 2)]) + ' '
+    print(side1)
+    print(res)
+    print(side1)
 
-    return f'''
-To: {mail}
-Приветствую, {name}!
-Вам назначен экзамен, который пройдет {date}, в {time}. 
-По адресу: {place}. 
-Экзамен будет проводить, {teacher} в кабинете {number}.
-Желаем удачи на экзамене!'''
-           
 
-print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00', 'Часова 23, корпус 2'))
-print()
-print(generate_letter('lara@yandex.ru', 'Лариса', '10 декабря', '12:00',
-                      'Часова 23, корпус 2', 'Василь Ярошевич', 23))
+pretty_print([1, 2, 10, 23, 123, 3000])
+pretty_print(['abc', 'def', 'ghi', '12345'])
+pretty_print(['abc', 'def', 'ghi'], side='*')
+pretty_print(['abc', 'def', 'ghi'], delimiter='#')
+pretty_print(['abc', 'def', 'ghi'], side='*', delimiter='#')
