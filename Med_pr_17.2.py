70f74ca59a21018c0a8c33fa5af646165ba51b78
viktortatarnikov <vtok241098@mail.ru>
2023-07-04 12:08:12 +0300

    Med_pr_17.2.4.py

--- Med_pr_17.2.py
+++ Med_pr_17.2.py
@@ -1,8 +1,8 @@
-import random as r
-file = open('lines.txt', 'r', encoding='utf-8')
-print(r.choice(file.readlines()))
+file = open('numbers.txt', 'r', encoding='utf-8')
+nums = [int(num) for num in file.readlines()]
+print(sum(nums))
 
 file.close()
 
-# Прикольное решение
-# print(set(f.readlines()).pop())
+# Красивое решение
+# print(sum(map(int, file)))
