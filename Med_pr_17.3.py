b8ade53a86eb0d6db5fab8aa297e4f08af25cb98
viktortatarnikov <vtok241098@mail.ru>
2023-07-05 21:44:36 +0300

    Med_pr_17.3.6.py

--- Med_pr_17.3.py
+++ Med_pr_17.3.py
@@ -1,10 +1,12 @@
-with open('nums.txt', 'r', encoding='utf-8') as f:
-    a = f.read()
-    b = ''
-    for ind in a:
-        if ind.isdigit():
-            b += ind
-        else:
-            b += ' '
+with open('file.txt', 'r', encoding='utf-8') as f:
+    words = len(f.read().split())
+    f.seek(0)
+    kursor = f.tell()
+    pred_liters = list(map(str.strip, f.readlines()))
+    count = 0
+    for sentes in pred_liters:
+        for lit in sentes:
+            if lit.isalpha():
+                count += 1
 
-    print(sum(map(int, b.split())))
+    print(f"Input file contains:\n{count} letters\n{words} words\n{len(pred_liters)} lines")
