2e356b550d458c55c35738bc23fad1c3fa57ba7c
viktortatarnikov <vtok241098@mail.ru>
2023-07-05 15:37:16 +0300

    Med_pr_17.3.4.py

--- Med_pr_17.3.py
+++ Med_pr_17.3.py
@@ -1,3 +1,3 @@
-with open('lines.txt', 'r', encoding='utf-8') as f:
-    lines = [line.strip() for line in f]
-    print(*filter(lambda x: len(x) == len(max(lines, key=len)), lines), sep='\n')
+with open('numbers.txt', 'r', encoding='utf-8') as f:
+    lines = [num.strip() for num in f.readlines()]
+    print(*map(lambda x: sum([int(num) for num in x.split()]), lines), sep='\n')
