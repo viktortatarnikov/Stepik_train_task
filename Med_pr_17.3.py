aa6770707831db1ed24bb19b5f8016900462ac38
viktortatarnikov <vtok241098@mail.ru>
2023-07-05 21:54:27 +0300

    Med_pr_17.3.7.py

--- Med_pr_17.3.py
+++ Med_pr_17.3.py
@@ -1,12 +1,6 @@
-with open('file.txt', 'r', encoding='utf-8') as f:
-    words = len(f.read().split())
-    f.seek(0)
-    kursor = f.tell()
-    pred_liters = list(map(str.strip, f.readlines()))
-    count = 0
-    for sentes in pred_liters:
-        for lit in sentes:
-            if lit.isalpha():
-                count += 1
-
-    print(f"Input file contains:\n{count} letters\n{words} words\n{len(pred_liters)} lines")
+from random import choice
+with open('first_names.txt', 'r', encoding='utf-8') as ferst, open('last_names.txt', 'r', encoding='utf-8') as last:
+    names1 = list(map(str.strip, ferst.readlines()))
+    names2 = list(map(str.strip, last.readlines()))
+    for _ in range(3):
+        print(choice(names1), choice(names2))
