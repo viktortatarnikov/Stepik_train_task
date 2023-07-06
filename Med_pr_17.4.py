503714f2ae45cb157cc18ea8d690833deebae86c
viktortatarnikov <vtok241098@mail.ru>
2023-07-06 12:37:56 +0300

    Med_pe_17.4.2.py

--- Med_pr_17.4.py
+++ Med_pr_17.4.py
@@ -1,2 +1,4 @@
-with open('output.txt', 'w', encoding='utf-8') as file:
-    file.write(input())
+import random as r
+with open('random.txt', 'w', encoding='utf-8') as file:
+    for _ in range(25):
+        print(r.randint(111, 777), file=file)
