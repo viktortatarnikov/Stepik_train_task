bf18662ef868303fa64b9b7e21b76d53c710223c
viktortatarnikov <vtok241098@mail.ru>
2023-07-06 16:47:41 +0300

    Med_pe_17.4.7.py

--- Med_pr_17.4.py
+++ Med_pr_17.4.py
@@ -1,7 +1,10 @@
-with open('output.txt', 'w', encoding='utf-8') as outp:
-    filse = [input() for _ in range(int(input()))]
-    for fil in filse:
-        with open(fil, 'r', encoding='utf-8') as data:
-            dat = data.readlines()
-            for line in dat:
-                print(line, file=outp, end='')
+def minutes(x):
+    res=[int(i) for i in x.split(':')]
+    return res[0]*60+res[1]
+
+with open('output.txt', 'w', encoding='utf-8') as outp, open('logfile.txt', 'r', encoding='utf-8') as inp:
+    data = inp.readlines()
+    for dat in data:
+        name, fers_t, last_t = dat.strip().split(', ')
+        if minutes(last_t) - minutes(fers_t) >= 60:
+            print(name, file=outp)
