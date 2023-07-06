2844d9c88699d6193c2f5e5e818b575db8cc362b
viktortatarnikov <vtok241098@mail.ru>
2023-07-06 15:54:34 +0300

    Med_pe_17.4.5.py

--- Med_pr_17.4.py
+++ Med_pr_17.4.py
@@ -1,7 +1,14 @@
-with open('class_scores.txt', 'r', encoding='utf-8') as inpt, open('new_scores.txt', 'w', encoding='utf-8') as outp:
-    for line in inpt.readlines():
-        name, skore = line.strip().split()
-        if int(skore) <= 95:
-            print(name+ ' ' + str(int(skore) + 5), file=outp)
-        else:
-            print(name + ' ' + '100', file=outp)
+from decimal import *
+with open('goats.txt', 'r', encoding='utf-8') as inpt, open('answer.txt', 'w', encoding='utf-8') as outp:
+    colors = list(set(filter(lambda x: ' goat' in x, map(str.strip, inpt.readlines()))))
+
+    inpt.seek(0)
+    al = list(map(str.strip, inpt.readlines()))
+
+    goat_dict = {key: 0 for key in colors}
+
+    for ind in range(al.index('GOATS') + 1, len(al)):
+        goat_dict[al[ind]] = goat_dict.get(al[ind], 0) + 1
+    for key, value in sorted(goat_dict.items()):
+        if Decimal(value/(len(al) - al.index('GOATS'))) >= Decimal(7/100):
+            print(key, file=outp)
