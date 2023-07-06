22f33c3376770401abb9e5508dbe46556f09b408
viktortatarnikov <vtok241098@mail.ru>
2023-07-06 16:38:14 +0300

    Med_pe_17.4.6.py

--- Med_pr_17.4.py
+++ Med_pr_17.4.py
@@ -1,14 +1,7 @@
-from decimal import *
-with open('goats.txt', 'r', encoding='utf-8') as inpt, open('answer.txt', 'w', encoding='utf-8') as outp:
-    colors = list(set(filter(lambda x: ' goat' in x, map(str.strip, inpt.readlines()))))
-
-    inpt.seek(0)
-    al = list(map(str.strip, inpt.readlines()))
-
-    goat_dict = {key: 0 for key in colors}
-
-    for ind in range(al.index('GOATS') + 1, len(al)):
-        goat_dict[al[ind]] = goat_dict.get(al[ind], 0) + 1
-    for key, value in sorted(goat_dict.items()):
-        if Decimal(value/(len(al) - al.index('GOATS'))) >= Decimal(7/100):
-            print(key, file=outp)
+with open('output.txt', 'w', encoding='utf-8') as outp:
+    filse = [input() for _ in range(int(input()))]
+    for fil in filse:
+        with open(fil, 'r', encoding='utf-8') as data:
+            dat = data.readlines()
+            for line in dat:
+                print(line, file=outp, end='')
