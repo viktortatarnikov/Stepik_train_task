9c6000b203f666db81c5b6cb337bca49cb4cd005
viktortatarnikov <vtok241098@mail.ru>
2023-07-07 09:31:57 +0300

    Med_pr_18.3

--- Med_pr_18.py
+++ Med_pr_18.py
@@ -1,4 +1,7 @@
-with open('ledger.txt', 'r', encoding='utf-8') as file:
-    a = file.readlines()
-
-    print('$' + str(sum(map(lambda x: int(x.strip().lstrip('$')), a))))
+with open('grades.txt', 'r', encoding='utf-8') as file:
+    count = 0
+    for student in file.readlines():
+        if int(student.strip().split()[1]) >= 65 and int(student.strip().split()[2]) >= 65\
+                and int(student.strip().split()[3]) >=65:
+            count += 1
+    print(count)
