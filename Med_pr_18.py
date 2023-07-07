fca3fc12620b9ac770f5e3469888f2650906f18f
viktortatarnikov <vtok241098@mail.ru>
2023-07-07 09:40:07 +0300

    Med_pr_18.4

--- Med_pr_18.py
+++ Med_pr_18.py
@@ -1,7 +1,5 @@
-with open('grades.txt', 'r', encoding='utf-8') as file:
-    count = 0
-    for student in file.readlines():
-        if int(student.strip().split()[1]) >= 65 and int(student.strip().split()[2]) >= 65\
-                and int(student.strip().split()[3]) >=65:
-            count += 1
-    print(count)
+with open('words.txt', 'r', encoding='utf-8') as file:
+    words = file.read().strip().split()
+    for word in words:
+        if len(word) == len(max(words, key=len)):
+            print(word)
