17fb8ed9cbc246ccac5c44bc13e48192e0d81eb8
viktortatarnikov <vtok241098@mail.ru>
2023-07-07 10:22:32 +0300

    Med_pr_18.6

--- Med_pr_18.py
+++ Med_pr_18.py
@@ -1,13 +1,12 @@
-with open(input(), 'r', encoding='utf-8') as file:
-    tale = file.readlines()[:-11:-1]
-    for line in tale[::-1]:
-        print(line.strip())
-
-
-# Интересное решение
-# txt = []
-#     for line in file:
-#         txt += [line.strip()]
-#         if len(txt) > 10:
-#             del txt[0]
-#     print(*txt, sep='\n')
+with open('forbidden_words.txt', 'r', encoding='utf-8') as forb, open(input(), 'r', encoding='utf-8') as file:
+    bed_words = forb.read().strip().split()
+    line_old = file.read()
+    line_lower = line_old.lower()
+    for word in bed_words:
+        if word in line_lower:
+            line_lower = line_lower.replace(word, '*'*len(word))
+    for ind in range(len(line_lower)):
+        if line_lower[ind] == '*':
+            print('*', end='')
+        else:
+            print(line_old[ind], end='')
